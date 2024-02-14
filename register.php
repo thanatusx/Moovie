eada5877f0c14585f99e023c8f9268e751aaa751
Clebto <mastergu657@gmail.com>
2024-02-14 20:15:43 -0300

    main

--- register.php
+++ register.php
@@ -1,133 +1,55 @@
 <?php
     include("conexao.php");
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="images/icons/logomoovie.ico">
-
-    <!-- BIBLIOTECAS PERSONALIZADAS -->
-    <link
-        rel="stylesheet" 
-        href="style/header.css">
-
-    <link
-        rel="stylesheet" 
-        href="style/register.css">
-
-    <!-- BIBLIOTECA BOOTSTRAP -->
-    <link
-        rel="stylesheet" 
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
-        crossorigin="anonymous">
-    
-    <!-- BIBLIOTECA FONT AWESOME -->
-    <link 
-        rel="stylesheet" 
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
-        crossorigin="anonymous" 
-        referrerpolicy="no-referrer"/>
-
-    <title>Registro | MOOVIE</title>
-</head>
-<body>
-    <section class="register-form" style="background-image: url(images/background/banner.svg);">
-        <div class="formlogin-container">
-            <div id="titulologin">
-                <h1 class="text-center">CADASTRO</h1>
-            </div>
-            <div class="text-center" id="user-error">Usuário já está sendo usado.</div>
-            <div class="text-center" id="email-error">Email já está sendo usado.</div>
-            <div class="text-center" id="senha-error">As senhas estão diferentes.</div>
-
-            <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST" onsubmit="return validarSenha();">
-                <label id="lblusuario" for="usuario">Usuário:</label>
-                <div class="text-center">
-                    <input class="inputtxt" type="text" id="usuario" placeholder="Usuário..." name="user" autocomplete="off" required>
-                </div>
-
-                <label id="lblemail">E-mail:</label>
-                <div class="text-center">
-                    <input class="inputtxt" type="email" id="email" placeholder="Email..." name="email" autocomplete="off" required>
-                </div>
-
-                <label class="lblsenha"for="senha">Senha:</label>
-                <div class="text-center">
-                    <input class="inputtxt" type="password" id="senha" placeholder="Senha..." name="password" required>
-                </div>
-
-                <label class="lblsenha" for="confirmarSenha">Confirmar Senha:</label>
-                <div class="text-center">
-                    <input class="inputtxt" type="password" id="confirmarsenha" placeholder="Confirmar senha..."required>
-                </div>
-                <div class="text-center botoes">
-                    <input class="" id="submit" type="submit" value="Cadastrar">
-                </div>
-            </form>
-        </div>
-    </section>
-
-    <!-- BIBLIOTECA PERSONALIZADA -->
-    <script src="script/header.js"></script>
-    <script src="script/register.js"></script>
-
-    <!-- BOOTSTRAP JS -->
-    <script 
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous">
-    </script>
-
-    <!-- FONT AWESOME JS -->
-    <script 
-        src="https://kit.fontawesome.com/a2edaa36f8.js"
-        crossorigin="anonymous">
-    </script>
-</body>
-</html>
-
-<?php 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $username = $_POST['user'];
         $password = $_POST['password'];
         $email = $_POST['email'];
     
-        $sql = "SELECT COUNT(*) AS count_usuario FROM usuarios WHERE usuario = :usuario";
-        $usuarioExistente = ($resultado['count_usuario'] > 0);
-        
-        $sql = "SELECT COUNT(*) AS count_email FROM usuarios WHERE email = :email";
-        $statement = $conexao->prepare($sql);
-        $statement->bindParam(':email', $email);
-        $statement->execute();
-        $resultado = $statement->fetch();
-        
-        $emailExistente = ($resultado['count_email'] > 0);
-        
-        if ($usuarioExistente || $emailExistente) {
-            $erroHtml = '';
-            if ($usuarioExistente) {
-                header('Location: cadastro.html?error=1');
+        $sqluser = "SELECT COUNT(*) AS count_user FROM users WHERE user = '$username'";
+        $resultuser = mysqli_query($conn, $sqluser);
+        if ($resultuser) {
+            $row = mysqli_fetch_assoc($resultuser);
+            if ($row['count_user'] > 0) {
+                $usuarioexistente = true;
+            } else {
+                $usuarioexistente = false;
             }
-            if ($emailExistente) {
-                header('Location: cadastro.html?error=2');
-        
+        } else {
+            echo "Erro ao executar a consulta do usuario: " . mysqli_error($conn);
+        }
+
+        $sqlemail = "SELECT COUNT(*) AS count_email FROM users WHERE email = '$email'";
+        $resultemail = mysqli_query($conn, $sqlemail);
+        if ($resultemail) {
+            $row = mysqli_fetch_assoc($resultemail);
+            if ($row['count_email'] > 0) {
+                $emailexistente = true;
+            } else {
+                $emailexistente = false;
             }
-            echo $erroHtml;
+        } else {
+            echo "Erro ao executar a consulta do email: " . mysqli_error($conn);
+        }
+        
+        if ($usuarioexistente || $emailexistente) {
+            if ($usuarioexistente) {
+                header('Location: register.html?error=1');
+            }
+            if ($emailexistente) {
+                header('Location: register.html?error=2');
+            }
             exit;
         }
 
         $hash = password_hash($password, PASSWORD_DEFAULT);
         $sql = "INSERT INTO users (user, email, password) VALUES ('$username', '$email', '$hash')";
-    
-        
+        try{
             mysqli_query($conn, $sql);
-            echo "User is registered";
-        
+            header('Location: login.html');
+            echo "a";
+        }catch(mysqli_sql_exception){
+            header('Location: register.html?error=3');
+        }
     }
 
 mysqli_close($conn); ?>
--- script/register.js
+++ script/register.js
@@ -9,17 +9,38 @@ function validarSenha() {
     return true;
   }
 
-  window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
     const urlParams = new URLSearchParams(window.location.search);
     const error = urlParams.get('error');
-    if (error == '1') {
-      const userError = document.getElementById('user-error');
-      userError.style.display = 'block';
-      history.replaceState(null, '', window.location.pathname);
+    if(error == 1){
+        const userError = document.getElementById('user-error');
+        userError.style.display = 'block';
+        history.replaceState(null, '', window.location.pathname);
     }
-    if (error == '2') {
-      const emailUser = document.getElementById('email-error');
-      emailUser.style.display = 'block';
-      history.replaceState(null, '', window.location.pathname);
+    if(error == 2){
+        const emailUser = document.getElementById('email-error');
+        emailUser.style.display = 'block';
+        history.replaceState(null, '', window.location.pathname);
     }
-  });
+    if (error == 3){
+        const sqlError = document.getElementById('sql-error');
+        sqlError.style.display = 'block';
+        history.replaceState(null, '', window.location.pathname);
+    }
+});
+
+let showPassword = document.getElementById("eye");
+let senha = document.getElementById("senha");
+
+showPassword.onclick = function(){
+    if(senha.type == "password"){
+        senha.type = "text";
+        this.setAttribute('name','eye-off-outline');
+    }else{
+        senha.type = "password";
+        this.setAttribute('name','eye-outline');
+    }
+}
+
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required')
